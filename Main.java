67010f49005358c9be4791c476406571fbead012
abeykoon552-prog <abeykoon552@gmail.com>
2026-06-07 17:49:22 +0530

    Add Main.java with menu and student management  methods

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main{
+	static ArrayList<Student> studentList=new ArrayList<>();
+	static Scanner sc= new Scanner(System.in);
+	
+	public static void main(String[] args){
+		int option;
+		do{
+			System.out.println("----STUDENT GRADE MANAGEMENT SYSTEM---0");
+			System.out.println("1.Add a Student");
+      System.out.println("2.Display All Students");
+      System.out.println("3.Search Student By Id");
+      System.out.println("4.Calculate Average Marks");
+      System.out.println("5.Exit");
+      System.out.print("Enter your Option: ");
+      option=sc.nextLine();
+      sc.nextLine();
+
+      
+
+      
